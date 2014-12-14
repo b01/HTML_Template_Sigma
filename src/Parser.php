997e64b508868b1258e5585aa07055b86bb2180b
Khalifah Shabazz <shabazzk@gmail.com>
2014-12-14 10:10:08 -0500

    Add core class.

---
+++ src/Parser.php
@@ -0,0 +1,13 @@
+<?php namespace Kshabazz\Sigma;
+/**
+ * Implementation of Integrated Templates API with template 'compilation' added.
+ *
+ * @copyright 2014
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ * @link      http://pear.php.net/package/HTML_Template_Sigma
+ */
+class Parser
+{
+
+}
+?>
