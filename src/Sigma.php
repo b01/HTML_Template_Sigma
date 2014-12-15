8d8a6ea1382aa7151c79b7a0a1a70de84ecfc204
Khalifah Shabazz <shabazzk@gmail.com>
2014-12-14 23:25:33 -0500

    Update Sigma doc-blocks.

--- src/Sigma.php
+++ src/Sigma.php
@@ -1,110 +1,11 @@
 <?php namespace Kshabazz\Sigma;
-/**
- * Implementation of Integrated Templates API with template 'compilation' added.
- *
- * PHP versions 4 and 5
- *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
- *
- * @category  HTML
- * @package   HTML_Template_Sigma
- * @author    Ulf Wendel <ulf.wendel@phpdoc.de>
- * @author    Alexey Borzov <avb@php.net>
- * @copyright 2001-2007 The PHP Group
- * @license   http://www.php.net/license/3_01.txt PHP License 3.01
- * @link      http://pear.php.net/package/HTML_Template_Sigma
- */
 
 /**
-* Implementation of Integrated Templates API with template 'compilation' added.
-*
-* The main new feature in Sigma is the template 'compilation'. Consider the
-* following: when loading a template file the engine has to parse it using
-* regular expressions to find all the blocks and variable placeholders. This
-* is a very "expensive" operation and is definitely an overkill to do on
-* every page request: templates seldom change on production websites. This is
-* where the cache kicks in: it saves an internal representation of the
-* template structure into a file and this file gets loaded instead of the
-* source one on subsequent requests (unless the source changes, of course).
-*
-* While HTML_Template_Sigma inherits PHPLib Template's template syntax, it has
-* an API which is easier to understand. When using HTML_Template_PHPLIB, you
-* have to explicitly name a source and a target the block gets parsed into.
-* This gives maximum flexibility but requires full knowledge of template
-* structure from the programmer.
-*
-* Integrated Template on the other hands manages block nesting and parsing
-* itself. The engine knows that inner1 is a child of block2, there's
-* no need to tell it about this:
-*
-* <pre>
-* + __global__ (hidden and automatically added)
-*     + block1
-*     + block2
-*         + inner1
-*         + inner2
-* </pre>
-*
-* To add content to block1 you simply type:
-* <code>$tpl->setCurrentBlock("block1");</code>
-* and repeat this as often as needed:
-* <code>
-*   $tpl->setVariable(...);
-*   $tpl->parseCurrentBlock();
-* </code>
-*
-* To add content to block2 you would type something like:
-* <code>
-* $tpl->setCurrentBlock("inner1");
-* $tpl->setVariable(...);
-* $tpl->parseCurrentBlock();
-*
-* $tpl->setVariable(...);
-* $tpl->parseCurrentBlock();
-*
-* $tpl->parse("block2");
-* </code>
-*
-* This will result in one repetition of block2 which contains two repetitions
-* of inner1. inner2 will be removed if $removeEmptyBlock is set to true (which
-* is the default).
-*
-* Usage:
-* <code>
-* $tpl = new HTML_Template_Sigma( [string filerootdir], [string cacherootdir] );
-*
-* // load a template or set it with setTemplate()
-* $tpl->loadTemplatefile( string filename [, boolean removeUnknownVariables, boolean removeEmptyBlocks] )
-*
-* // set "global" Variables meaning variables not beeing within a (inner) block
-* $tpl->setVariable( string variablename, mixed value );
-*
-* // like with the HTML_Template_PHPLIB there's a second way to use setVariable()
-* $tpl->setVariable( array ( string varname => mixed value ) );
-*
-* // Let's use any block, even a deeply nested one
-* $tpl->setCurrentBlock( string blockname );
-*
-* // repeat this as often as you need it.
-* $tpl->setVariable( array ( string varname => mixed value ) );
-* $tpl->parseCurrentBlock();
-*
-* // get the parsed template or print it: $tpl->show()
-* $html = $tpl->get();
-* </code>
-*
-* @category HTML
-* @package  HTML_Template_Sigma
-* @author   Ulf Wendel <ulf.wendel@phpdoc.de>
-* @author   Alexey Borzov <avb@php.net>
-* @license  http://www.php.net/license/3_01.txt PHP License 3.01
-* @version  Release: @package_version@
-* @link     http://pear.php.net/package/HTML_Template_Sigma
-*/
+ * Class Sigma
+ *
+ * @version  Release: @package_version@
+ * @package Kshabazz\Sigma
+ */
 class Sigma
 {
     /**
