da6d56c73a7175ca6a29eb870423c9563df1af46
Khalifah Shabazz <shabazzk@gmail.com>
2014-12-14 23:24:49 -0500

    Update PHP unit bootstrap file.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,17 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+// Load composer auto-loader.
+require_once __DIR__
+	. DIRECTORY_SEPARATOR . '..'
+	. DIRECTORY_SEPARATOR . 'vendor'
+	. DIRECTORY_SEPARATOR . 'autoload.php';
+
+\define( 'FIXTURES_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'fixtures' );
+
 $_HTML_Template_Sigma_cache_dir = __DIR__ . DIRECTORY_SEPARATOR . 'cache';
 $_HTML_Template_Sigma_templates_dir = __DIR__ . DIRECTORY_SEPARATOR . 'templates';
-$cacheDirExists = is_dir($_HTML_Template_Sigma_cache_dir);
-if (!$cacheDirExists)
+$cacheDirExists = \is_dir( $_HTML_Template_Sigma_cache_dir );
+if ( !$cacheDirExists )
 {
-	mkdir($_HTML_Template_Sigma_cache_dir);
+	\mkdir( $_HTML_Template_Sigma_cache_dir );
 }
 ?>
