e850504063e72760aac38cbf9025d8379b6c563e
Khalifah K. Shabazz <shabazzk@gmail.com>
2015-06-20 10:16:27 -0400

    Add file comment docblock to Parser class.

--- src/Parser.php
+++ src/Parser.php
@@ -1,4 +1,11 @@
 <?php namespace Kshabazz\Sigma;
+/**
+ * This is the core Sigma class which processes template files by way of parsing/setting blocks, placeholders, and
+ * handling of caching. This class is what a developer will generally instantiate in their code.
+ *
+ * Originally this was Sigma/Sigma, but has been rewritten to break out core pieces, in an effort to make it more
+ * readable. In the hopes that it will be simpler to maintain and improve as time goes on.
+ */
 
 /**
  * Class Parser
